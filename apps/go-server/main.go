51ecf30db8a86f8956b3399372aa8e95058d7f2e
Kanak <kanakkholwal@gmail.com>
2025-04-21 20:13:49 +0530

    feat: initialize Go server with middleware and routing setup

---
+++ apps/go-server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+
+	"github.com/kanakkholwal/go-server/middleware"
+	"github.com/kanakkholwal/go-server/routes"
+)
+
+func main() {
+	godotenv.Load()
+
+	app := fiber.New()
+
+	app.Use(middleware.ErrorHandler)
+	app.Use(middleware.CustomCORS)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Welcome to the server!",
+			"status":  "healthy",
+		})
+	})
+
+	routes.RegisterRoutes(app.Group("/api"))
+
+	log.Fatal(app.Listen(":8080"))
+}
