34c56d0ad9413383cfd09dd943d09560de4d2f15
Kanak <kanakkholwal@gmail.com>
2025-06-05 16:33:39 +0530

    Remove unnecessary newline in mailerPayloadSchema definition

--- apps/mail-server/types/schema.ts
+++ apps/mail-server/types/schema.ts
@@ -19,7 +19,6 @@ export const mailerPayloadSchema = z.object({
     to: z.array(z.string().email()),
     subject: z.string(),
     html: z.string(),
-    
 })
 
 export type MailerPayload = z.infer<typeof mailerPayloadSchema>
