5ba6980d5fc56b6f85cab5100aee9a3747db4eda
Kanak <kanakkholwal@gmail.com>
2025-06-05 22:39:51 +0530

    refactor: simplify hostel page layout by removing conditional rendering for banned users

--- apps/platform/app/[moderator]/(student)/outpass/layout.tsx
+++ apps/platform/app/[moderator]/(student)/outpass/layout.tsx
@@ -1,39 +1,29 @@
 import { HostelDetailsForNonAdmins } from "@/components/application/hostel/hostel-details";
 import EmptyArea from "@/components/common/empty-area";
 import { Separator } from "@/components/ui/separator";
-import ConditionalRender from "@/components/utils/conditional-render";
 import { LuBuilding } from "react-icons/lu";
 import { getHostelByUser } from "~/actions/hostel";
 
 export default async function HostelPageLayout(props: {
   children: React.ReactNode;
 }) {
-  const { success, message, hostel, hosteler } = await getHostelByUser();
+  const response = await getHostelByUser();
 
-  if (!success || !hostel || !hosteler) {
+  if (!response.success || !response.hostel) {
     return (
       <EmptyArea
         icons={[LuBuilding]}
         title="No Hostel Found for this user"
-        description={message}
+        description={response.message}
       />
     );
   }
 
   return (
     <div className="space-y-5">
-      <HostelDetailsForNonAdmins hostel={hostel} />
+      <HostelDetailsForNonAdmins hostel={response.hostel} />
       <Separator />
-      <ConditionalRender condition={hosteler?.banned}>
-        <EmptyArea
-          icons={[LuBuilding]}
-          title="You are banned from requesting outpass for the following reason"
-          description={`${hosteler.bannedReason} till ${hosteler.bannedTill ? new Date(hosteler.bannedTill).toLocaleString() : "N/A"}`}
-        />
-      </ConditionalRender>
-      <ConditionalRender condition={!hosteler?.banned}>
-        {props.children}
-      </ConditionalRender>
+      {props.children}
     </div>
   );
 }
