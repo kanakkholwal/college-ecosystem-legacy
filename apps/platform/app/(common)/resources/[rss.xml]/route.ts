93194f9198bbce77b5e9d71c567bf54b1deed4c8
Kanak <kanakkholwal@gmail.com>
2025-06-26 15:29:47 +0530

    feat: add RSS feed generation for resources

---
+++ apps/platform/app/(common)/resources/[rss.xml]/route.ts
@@ -0,0 +1,38 @@
+import { getAllResources } from '~/lib/mdx'
+import { appConfig } from '~/project.config'
+
+export async function GET() {
+    const rssXml = await generateRssXml()
+    return new Response(rssXml, {
+        headers: {
+            'Content-Type': 'application/rss+xml'
+        }
+    })
+}
+
+async function generateRssXml() {
+    const baseUrl = appConfig.url.replace(/\/$/, '') // Ensure no trailing slash
+    const posts = (await getAllResources()).filter(post => Boolean(post.date))
+    const items = posts
+        .map(post => {
+            return `<item>
+      <title>${post.title}</title>
+      <link>${baseUrl}${`/resources/${post.type}/${post.slug}`}</link>
+      <guid>${baseUrl}${`/resources/${post.type}/${post.slug}`}</guid>
+      <description>${post.summary}</description>
+      <pubDate>${new Date(post.date!).toUTCString()}</pubDate>
+    </item>`
+        })
+        .join('')
+    return `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>nuqs</title>
+    <link>${baseUrl}/resources</link>
+    <description>URL state management with nuqs</description>
+    <atom:link href="http://${baseUrl}/resources/rss.xml" rel="self" type="application/rss+xml" />
+  ${items}
+  </channel>
+</rss>
+`
+}
